5b733e0d61ac003863203e755f1d2d8d234ad85d
remora00 <remora0@protonmail.com>
2021-11-21 18:21:30 +0545

    initial commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.0;
+
+contract Lottery {
+    // TODO
+
+    address payable[] public players;
+
+    function enter() public {}
+
+    function getEntranceFee() public {}
+
+    // managed by admin
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
