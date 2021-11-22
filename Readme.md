7235729f81b6c414c76a17d5d42d18eb2319d701
remora00 <remora0@protonmail.com>
2021-11-22 19:09:24 +0545

    add integration test

--- Readme.md
+++ Readme.md
@@ -6,6 +6,12 @@
 
 `brownie networks list`
 
-`brownie networks add development mainnet-fork cmd=ganache-cli host=http://127.0.0.1 fork=https://eth-mainnet.alchemyapi.io/v2/1jqF0mvR0Dl3pcHS2fjtecW1sopg8lb0 accounts=10 mnemonic=brownie port=8545`
+`brownie networks add development mainnet-fork cmd=ganache-cli host=http://127.0.0.1 fork=https://eth-mainnet.alchemyapi.io/v2/asdfasdfs accounts=10 mnemonic=brownie port=8545`
 
 `brownie test --network mainnet-fork`
+
+## Test in rinkeby network
+
+```bash
+brownie test -k test_can_pick_winner --network rinkeby -s
+```
--- tests/test_lottery_integration.py
+++ tests/test_lottery_integration.py
@@ -1,2 +1,25 @@
 from brownie import Lottery, accounts, network, config
 from web3 import Web3
+from scripts.useful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    fund_with_link,
+)
+import pytest
+import time
+from scripts.deploy_lottery import deploy_lottery
+
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(60)
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
